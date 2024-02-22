f910deb29b2ebc5a27c439d26c28eb11583af3aa
evereasy21 <evereasy21@gmail.com>
2024-02-22 11:34:58 +0800

    Initial commit

---
+++ src/main/java/com/faryuanway/happyshopping/HappyshoppingApplication.java
@@ -0,0 +1,13 @@
+package com.faryuanway.happyshopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyshoppingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappyshoppingApplication.class, args);
+	}
+
+}
